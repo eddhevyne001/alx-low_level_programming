522c7ea0d073b26d81853b34fcd585fe47dd7feb
eddhevyne001 <dhevyne001@gmail.com>
2023-06-15 16:31:45 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - hello printf
  * Return: 0
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
