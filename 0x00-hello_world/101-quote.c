80047be081b347d0d1acb19770576ad26952bd45
eddhevyne001 <dhevyne001@gmail.com>
2023-06-15 18:13:29 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main -  UNIX is basically a simple operating system
- * Return 1 (success)
+ * main - unix operating system
+ * Return: 1 (error)
  */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-return (1);
+	return (1);
 }
