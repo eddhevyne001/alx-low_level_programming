712509ec6c459c8aedfcd6620b1507dfa965a291
eddhevyne001 <dhevyne001@gmail.com>
2023-06-27 15:58:59 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - Pointer to the int and changes value to 98
+ * @n: an integer input pointer
+ * Return: void
+ */
+void reset_to_98(int *n)
+}
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - character c to stdout
+ * @c: print
+ * Return: success 1
+ * On error, -1 is returned, error is corrected
+ */
+int _putchar(char c)
+{
+        return(write(1, &c,1));
+}
