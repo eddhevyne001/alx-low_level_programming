3d9cbed77be4f00687c3ed8b7c492e2873628760
eddhevyne001 <dhevyne001@gmail.com>
2023-07-11 17:10:13 -0700

    libmy

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - gives the value of an integer.
+ * @c: the number to be computed.
+ * Return: The absolut value or zero
+ */
+
+int _abs(int c)
+
+{
+	if (c < 0)
+	{
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
