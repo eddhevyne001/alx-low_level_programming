072d7eb2c55e9992bbc445e4310253f48a825d84
eddhevyne001 <dhevyne001@gmail.com>
2023-07-18 16:23:43 -0700

    update

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of file
+ *
+ * Return: Success
+ */
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef SUM_H
+#define SUM_H
+
+int sum(int x, int y) 
+{
+	return x + y;
+}
+
+#endif // SUM_H
+
