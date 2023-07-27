68d29c1a61336812c8705d154cced2b1d1dd5cd8
eddhevyne001 <dhevyne001@gmail.com>
2023-07-27 16:18:10 -0700

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,50 +4,40 @@
  * add_node_end - Adds a new node at the end of a linked list.
  * @head: Pointer to the head of the linked list.
  * @str: String to be added to the linked list.
- *
  * Return: On success, returns the address of the newly added node.
- *         On failure or if `str` is NULL, returns NULL.
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-    /* Allocate memory for the new node */
-    list_t *new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
+	char *dup;
+	int len;
+	list_t *new, *last;
 
-    /* Duplicate the input string */
-    char *dup = strdup(str);
-    if (dup == NULL)
-    {
-        free(new);
-        return (NULL);
-    }
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 
-    /* Calculate the length of the string */
-    int len;
-    for (len = 0; str[len]; len++)
-        ;
+	dup = strdup(str);
+	if (str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
-    /* Fill in the new node with the data */
-    new->str = dup;
-    new->len = len;
-    new->next = NULL;
+	for (len = 0; str[len];)
+		len++;
 
-    /* If the list is empty, set the new node as the head */
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        /* Traverse the list to find the last node */
-        list_t *last = *head;
-        while (last->next != NULL)
-            last = last->next;
+	new->str = dup;
+	new->len = len;
+	new->next = NULL;
 
-        /* Add the new node to the end of the list */
-        last->next = new;
-    }
-
-    return (*head);
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
+	}
+	return (*head);
 }
-
